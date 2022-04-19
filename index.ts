d006e138ca4e5fecfd5afd78be0af00d9ac35d41
shanas <nassibshaban345@gmail.com>
2022-04-19 23:12:27 +0300

    learn: web sockets

--- index.ts
+++ index.ts
@@ -1,11 +1,30 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  return res.send("Hello World!");
+const __dirname = process.cwd();
+
+app.get("/", (req: any, res: any) => {
+  return res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(PORT, () => console.log("Server started"));
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("chat message", (msg: string) => {
+    console.log(`Message ${msg}`);
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(PORT, () => console.log("Server started"));
